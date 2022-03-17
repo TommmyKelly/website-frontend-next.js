2a65bf37a0647188c274e12ff80c3e34c95dd22f
TommmyKelly <tommy_kelly@icloud.com>
2022-03-17 05:15:16 +0000

    added mobile responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 import Head from "next/head";
-import Layout2 from "@/components/Layout2";
 
 function MyApp({ Component, pageProps }) {
   return (
